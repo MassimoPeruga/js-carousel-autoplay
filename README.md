0783ad06b877b7ef3c01d56ed74d7648189ef7e7
MassimoPeruga <spamma-mutevole0p@icloud.com>
2023-11-17 17:05:47 +0100

    feat: MIlestone

---
+++ README.md
@@ -0,0 +1,8 @@
+# Carosello Automatico
+
+Viene aggiunta la funzionalità di autoplay: dopo  3 secondi l’immagine attiva passa alla successiva. 
+
+1. Viene creata una nuova funzione per l'autoplay,
+2. Viene aggiunta una variabile per tracciare lo stato dell'autoplay,
+3. Viene usato setInterval per eseguire una funzione ad intervalli regolari, all'interno di questa funzione viene simulato il click del pulsante "next".
+4. Viene chiamata la funzione startAutoplay per avviare l'autoplay quando la pagina viene caricata.
---
+++ js/main.js
@@ -0,0 +1,133 @@
+'use strict';
+
+// Funzione per cambiare l'immagine
+let autoplayInterval;
+function changeImage() {
+    allItems[currentImage].classList.remove('ms_active');
+    allThumbnails[currentImage].classList.remove('ms_active_thumbnail');
+
+    if (currentImage < images.length - 1) {
+        currentImage++;
+    } else {
+        currentImage = 0;
+    }
+
+    allItems[currentImage].classList.add('ms_active');
+    allThumbnails[currentImage].classList.add('ms_active_thumbnail');
+}
+
+// Funzione per avviare l'autoplay
+function startAutoplay() {
+    autoplayInterval = setInterval(changeImage, 3000); // Cambia immagine ogni 3 secondi
+}
+// Avvia l'autoplay all'inizio
+startAutoplay();
+
+//Variabili
+const images = ["./img/01.jpg", "./img/02.jpg", "./img/03.jpg", "./img/04.jpg", "./img/05.jpg"];
+const itemsContainer = document.querySelector(".ms_items");
+const prev = document.querySelector('.ms_prev');
+const next = document.querySelector('.ms_next');
+
+let currentImage = 0;
+
+//Creazione della slide principale
+for (let i = 0; i < images.length; i++) {
+    //slide
+    const item = document.createElement("div");
+    item.classList.add("ms_item");
+
+    if (i === currentImage) {
+        item.classList.add("ms_active");
+    }
+
+    //img
+    const img = document.createElement("img");
+    img.src = `${images[i]}`;
+    img.alt = `Image ${i}`;
+    item.appendChild(img);
+    itemsContainer.appendChild(item);
+}
+
+//Crezaione delle thumbnails
+const thumbnailsContainer = document.querySelector(".ms_thumbnails");
+
+for (let i = 0; i < images.length; i++) {
+    const thumbnail = document.createElement("div");
+    thumbnail.classList.add("ms_thumbnail");
+
+    if (i === currentImage) {
+        thumbnail.classList.add("ms_active_thumbnail");
+    }
+
+    const thumbnailImg = document.createElement("img");
+    thumbnailImg.src = `${images[i]}`;
+    thumbnailImg.alt = `Thumbnail ${i}`;
+    thumbnail.appendChild(thumbnailImg);
+    thumbnailsContainer.appendChild(thumbnail);
+}
+
+
+//Gestione dei pulsanti
+const allItems = document.querySelectorAll(".ms_item");
+const allThumbnails = document.querySelectorAll(".ms_thumbnail");
+
+prev.addEventListener('click', function () {
+
+    allItems[currentImage].classList.remove('ms_active');
+    allThumbnails[currentImage].classList.remove('ms_active_thumbnail');
+
+    if (currentImage > 0) {
+        currentImage--;
+    }
+    else {
+        currentImage = images.length - 1
+    }
+
+    allItems[currentImage].classList.add('ms_active');
+    allThumbnails[currentImage].classList.add('ms_active_thumbnail');
+
+})
+
+next.addEventListener('click', function () {
+
+    allItems[currentImage].classList.remove('ms_active');
+    allThumbnails[currentImage].classList.remove('ms_active_thumbnail');
+
+    if (currentImage < images.length - 1) {
+        currentImage++;
+    }
+    else {
+        currentImage = 0
+    }
+
+    allItems[currentImage].classList.add('ms_active');
+    allThumbnails[currentImage].classList.add('ms_active_thumbnail');
+
+})
+
+// Bonus 3
+for (let i = 0; i < allThumbnails.length; i++) {
+    const currentThumbnail = allThumbnails[i];
+
+    currentThumbnail.addEventListener('click', function () {
+        let clickedThumbnail = i;
+
+        const activeThumbnail = document.querySelector('.ms_active_thumbnail');
+
+        if (activeThumbnail) {
+            activeThumbnail.classList.remove('ms_active_thumbnail');
+        }
+
+        currentThumbnail.classList.add('ms_active_thumbnail');
+        currentImage = clickedThumbnail;
+
+        for (let indiceItem = 0; indiceItem < allItems.length; indiceItem++) {
+            if (indiceItem === currentImage) {
+                allItems[indiceItem].classList.add('ms_active');
+            } else {
+                allItems[indiceItem].classList.remove('ms_active');
+            }
+        }
+    });
+}
